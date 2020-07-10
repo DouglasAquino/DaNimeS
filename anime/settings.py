6a6122c0f775819acaef594241c17eaff8486246
Stefan <stefan.luks@rede.ulbra.br>
2020-07-09 21:25:59 -0300

    alteração no template inicial e criação do cadastro de usuario

--- anime/settings.py
+++ anime/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL='inicial'
+LOGOUT_REDIRECT_URL='login'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- app_anime/urls.py
+++ app_anime/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import inicial
+from .views import inicial, cadastrar
 from django.conf.urls import url
 import audioop
 
 
 urlpatterns = [
     path('',inicial,name="inicial"),
+    path('cadastrar',cadastrar,name="cadastrar"),
     # Here we mount the app under /music. Feel free to use something else
     #url(r'^$', include(' '), name='home'),
     #url("risada", include("audiotracks.urls")),
